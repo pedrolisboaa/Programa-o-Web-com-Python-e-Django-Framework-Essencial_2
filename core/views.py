4a79448c2b53d5fe39c15f6d60f444e4e47ec88a
pedrolisboaa <phlisboa2000@gmail.com>
2023-03-15 22:30:56 -0300

    Finalizando a parte de formulário de contato

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .forms import ContatoForm
 
 # Create your views here.
@@ -7,8 +8,22 @@ def index(request):
     return render(request, 'index.html')
 
 def contato(request):
-    form = ContatoForm()
+    form = ContatoForm(request.POST or None)
     
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+            
+            print(f'Mensagem enviada')
+            print(nome, email, assunto, mensagem)
+            
+            messages.success(request, 'E-mail enviando com sucesso.')
+            form = ContatoForm()    
+    else:
+        messages.error(request, 'Erro ao enviar e-mai.')
     
     return render(request, 'contato.html', {'form': form})
 
